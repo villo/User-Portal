0d80b991d3aa0cd13396f11698a57f2d14accf3e
villo <jordan@villo.me>
2012-02-23 20:59:13 -0800

    Added alert to friends page. Added base friends page components, which shows the usernames of all of the friends.

--- source/pages/friends.js
+++ source/pages/friends.js
@@ -2,12 +2,54 @@ enyo.kind({
 	name: "friendsPage",
 	kind: "Page",
 	components: [
-		{classes: "hero-unit", components: [
-			{tag: "h1", content: "Welcome to Friends"},
-			{tag: "p", content: "Your favorite cloud service, better than ever. Villo 1.0 is coming soon, and it adds in tons of new features for developers and users alike. Not only will we bringing you a new version of Villo, we're also going to launch a brand new user website, and developer portal. We hope you like it."},
-			{tag: "p", components: [
-				{content: "Learn More", tag: "a", classes: "btn btn-primary btn-large", attributes: {"href": "http://villo.net/more/"}}
-			]}
-		]},
+		{kind: "Alert", content: 'You can add a friend by searching for their profile, then pressing the "Add Friend" button.', type: "info"},
+		{kind: "friendsModule", columns: 3}
 	]
-})
+});
+
+enyo.kind({
+	name: "friendsModule",
+	kind: "Control",
+	classes: "row",
+	published: {
+		columns : 4
+	},
+	components: [
+	],
+	columnSpan: null,
+	
+	create: function(){
+		this.inherited(arguments);
+		
+		//Set up number of columns:
+		this.columnSpan = Math.floor(12 / this.columns);
+		villo.friends.get({
+			callback: enyo.bind(this, this.getFriends)
+		});
+	},
+	getFriends: function(inSender){
+		if(inSender && inSender.friends){
+			enyo.forEach(inSender.friends, this.renderFriends, this);
+		}
+		this.render();
+	},
+	renderFriends: function(inSender){
+		this.createComponent({kind: "friendsItem", classes: "span" + this.columnSpan, username: inSender});
+	}
+});
+
+enyo.kind({
+	name: "friendsItem",
+	kind: "Control",
+	published: {
+		username: ""
+	},
+	components: [
+		{name: "username", content: ""}
+	],
+	create: function(){
+		this.inherited(arguments);
+		
+		this.$.username.setContent(this.username);
+	}
+});
