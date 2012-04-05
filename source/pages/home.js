1109a7b18093e4d5b96b7be7eb787362a50c4f02
villo <jordan@villo.me>
2012-04-04 19:22:29 -0700

    Updated home page UI to include multiple elements, more controls, and avatars. Added customizable properties to the home page UI kind.

--- source/pages/home.js
+++ source/pages/home.js
@@ -3,8 +3,9 @@ enyo.kind({
 	kind: "Page",
 	components: [
 		{classes: "row", components: [
-			{kind: "homePageItem"},
-			{kind: "homePageItem"}
+			{kind: "homePageItem", content: "Test 1, with a little content.", username: "kesne"},
+			{kind: "homePageItem", content: "Test 2"},
+			{kind: "homePageItem", content: "Test 3, with some more content."}
 		]}
 	]
 });
@@ -13,22 +14,48 @@ enyo.kind({
 	name: "homePageItem",
 	kind: "Control",
 	published: {
-		"span": 6
+		"span": 6,
+		"content": "",
+		"username": "Guest"
 	},
 	components: [
-		{classes: "pull-right btn-group", components: [
-			{kind: "Button", classes: "btn btn-mini dropdown-toggle", attributes: {"data-toggle": "dropdown"}, components: [
-				{tag: "span", classes: "caret"}
+		{classes: "row-fluid", components: [
+			{classes: "span2", components: [
+				{classes: "thumbnail", name: "avatar", style: "height: 50px; width: 50px;", tag: "img", src: "source/img/ajax-loader.gif"}
 			]},
-			{tag: "ul", classes: "dropdown-menu", components: [
-				{content: "Hi"}
-			]}
-		]},
-		{content: "Kesne", tag: "h3"},
-		{classes: "well", content: "Look, I'm in a a well!"}
+			{classes: "span10", components: [
+				{classes: "pull-right btn-group", components: [
+					{kind: "Button", classes: "btn btn-mini dropdown-toggle", attributes: {"data-toggle": "dropdown"}, components: [
+						{tag: "span", classes: "caret"}
+					]},
+					{tag: "ul", classes: "dropdown-menu", components: [
+						{tag: "li", components: [
+							{tag: "a", content: "View Profile", onclick: "viewProfile"}
+						]},
+						{tag: "li", components: [
+							{tag: "a", content: "Hide Post", onclick: "hidePost"}
+						]},
+						{tag: "li", classes: "divider"},
+						{tag: "li", components: [
+							{tag: "a", content: "View App", onclick: "hidePost"}
+						]},
+					]}
+				]},
+				{content: "", name: "username", tag: "h3"},
+				{classes: "well", name: "content", content: ""}
+			]},
+		]}
 	],
+	hidePost: function(){
+		//Animate it out:
+		//Then remove it completely: 
+		this.destroy();
+	},
 	create: function(){
 		this.inherited(arguments);
 		this.addClass("span" + this.span);
+		this.$.content.setContent(this.content);
+		this.$.username.setContent(this.username);
+		this.$.avatar.setSrc("https://api.villo.me/avatar.php?thumbnail=true&username=" + escape(this.username));
 	}
 });
